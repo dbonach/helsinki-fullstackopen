f2b0f961a09d12c0ae1c5db4287425b341db3c53
dbonach <deivitlopes@gmail.com>
2021-07-29 20:21:06 -0300

    Refactor part4, blog list
    Create 'app.js' and use it to set all express's instance
    configurations.

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,39 +1,6 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const Blog = require('./models/blog')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+const app = require('./app')
 const config = require('./utils/config')
-const blogRouter = require('./controllers/blog-list')
-
-
-let mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
