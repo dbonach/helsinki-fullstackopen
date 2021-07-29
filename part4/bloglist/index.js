916c7ae76bce2fd355a9431df306e3f945e3b6ea
dbonach <deivitlopes@gmail.com>
2021-07-29 16:09:22 -0300

    Refactor part4, blog list
    Create 'utils/logger.js' file to handle console.log and console.erro.
    Create 'utils/middleware.js' file and add requestLogger, unknownEndpoint
    and errorHandler functions to it.
    Create 'models/blog.js' file to handle the database model/schema.
    
    Implement all new functions into 'index.js'

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const Blog = require('./models/blog')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 
 let mongoUrl = process.env.MONGODB_URI
@@ -15,24 +17,26 @@ mongoose.connect(mongoUrl, {
   useCreateIndex: true
 })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.info('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog
@@ -40,9 +44,13 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => next(error))
 })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
