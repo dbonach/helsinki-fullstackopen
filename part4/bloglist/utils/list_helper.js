0f7813dfa3e590477c0a4e392490856d7cc13f6f
dbonach <deivitlopes@gmail.com>
2021-07-29 22:35:17 -0300

    Part4, ex-4.4, refactor totalLikes in list_helper

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,14 +4,20 @@ const dummy = (blog) => {
 }
 
 const totalLikes = (blogs) => {
-  let likesArray = blogs.map(b => b.likes)
+  // let likesArray = blogs.map(b => b.likes)
 
-  let likesTotal = likesArray.reduce(
-    (total, currentLike) => total + currentLike, 0
-  )
+  // let likesTotal = likesArray.reduce(
+  //   (total, currentLike) => total + currentLike, 0
+  // )
+  let likesTotal = blogs.reduce(
+    (total, currentBlog) => total + currentBlog.likes, 0)
 
   return likesTotal
 }
+
+// const favoriteBlog = (blogs) => {
+
+// })
 
 module.exports = {
   dummy,
