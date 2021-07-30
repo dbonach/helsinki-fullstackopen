10964a85095784d04d3845df70a852d94373b36c
dbonach <deivitlopes@gmail.com>
2021-07-30 09:22:08 -0300

    Add test for author with the most number of blogs
    Ex-4.6, helper functions and unit tests, step4
    
    Add helper function and test to check the author
    with the most number of blog posts among an array
    of blog posts.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -20,8 +20,33 @@ const favoriteBlog = (blogs) => {
   return blog
 }
 
+const mostBlogs = (blogs) => {
+  // reduce the array of objects to an array with only author names
+  let authors = blogs.map(blog => blog.author)
+
+  // reduce author's array to an object with
+  // their names and number of blogs
+  const occurrences = authors.reduce((obj, author) => {
+    obj[author] = (obj[author] || 0) + 1
+    return obj
+  }, {})
+
+  // isolate the author with the greatest number of blogs
+  let max = 0
+  let mostAuthor = ''
+  for (let author in occurrences) {
+    if (occurrences[author] > max) {
+      max = occurrences[author]
+      mostAuthor = author
+    }
+  }
+
+  return { author: mostAuthor, blogs: max }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
