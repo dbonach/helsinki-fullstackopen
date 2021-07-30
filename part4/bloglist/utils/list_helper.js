1e9841de064ed3a3c8fa58f9095491c97650c26c
dbonach <deivitlopes@gmail.com>
2021-07-29 22:28:27 -0300

    Add tests to count total number of likes
    Ex-4.4, helper functions and unit tests, step2
    
    Add helper function and tests to count the number
    of likes in a list of blog posts.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blog) => {
+  console.log(blog)
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likesArray = blogs.map(b => b.likes)
+
+  let likesTotal = likesArray.reduce(
+    (total, currentLike) => total + currentLike, 0
+  )
+
+  return likesTotal
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
