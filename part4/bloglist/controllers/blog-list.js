dbe2fef6ebec0b9073c964ba88ab08724392ec42
dbonach <deivitlopes@gmail.com>
2021-07-31 22:36:35 -0300

    Add more endpoints

--- part4/bloglist/controllers/blog-list.js
+++ part4/bloglist/controllers/blog-list.js
@@ -4,19 +4,41 @@ require('express-async-errors')
 
 
 blogRouter.get('/', async (request, response) => {
-
   const blogs = await Blog.find({})
   response.json(blogs)
-
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-    .catch(error => next(error))
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog
+    .findByIdAndUpdate(request.params.id, blog, { new: true })
+
+  response.json(updatedBlog)
 })
 
 module.exports = blogRouter
