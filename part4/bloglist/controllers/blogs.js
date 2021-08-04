464f1844e2bdcbc0099d3fc37ab7588e0726a712
dbonach <deivitlopes@gmail.com>
2021-08-04 14:56:50 -0300

    Refactor token extraction to its own middleware
    Move token extraction from 'controller/blogs'
    to 'utils/middleware', modify it to save the
    extracted token to the resquest object instead
    of returning a token.

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,13 +4,13 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 require('express-async-errors')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 
 blogRouter.get('/', async (request, response) => {
@@ -31,10 +31,8 @@ blogRouter.get('/:id', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const token = getTokenFrom(request)
-
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
