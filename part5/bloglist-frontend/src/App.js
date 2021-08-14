36893c6a0ae114eb4fbd676b3cefb5ac5145d9b9
dbonach <deivitlopes@gmail.com>
2021-08-14 20:57:05 -0300

    Add login service, refactor App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,22 +1,58 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
+import Login from './components/Login'
+import User from './components/User'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [loginData, setLoginData] = useState({ username: '', password: '' })
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const user = await loginService.login(loginData)
+      if (user) {
+        setUser(user)
+        setLoginData({ username: '', password: '' })
+      }
+    } catch (exception) {
+      console.error('Error: ', exception)
+    }
+  }
+
+  const loggedOutView = () => {
+    return (
+      <Login
+        loginData={loginData}
+        setLoginData={setLoginData}
+        handleLogin={handleLogin}
+      />
+    )
+  }
+
+  const loggedInView = () => {
+    return (
+      <div>
+        <User name={user.username} setUser={setUser} />
+        <Blogs blogs={blogs} />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+    <div className='wrapper'>
+      {user ? loggedInView() : loggedOutView()}
     </div>
   )
 }
