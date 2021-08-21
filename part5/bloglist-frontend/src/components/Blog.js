8d598073c07e1fbcc16fc07455eb6f5e756d8d74
dbonach <deivitlopes@gmail.com>
2021-08-21 00:17:31 -0300

    Add button to display all info of a blog
    Also add style to the element to be displayed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
 
+const previewInfoStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginBottom: '.25rem'
+}
+
+const fullInfoStyle = {
+  flexDirection: 'column',
+  marginBottom: '1rem',
+  border: '1px solid #ddd',
+  borderRadius: '.25rem',
+  padding: '.5rem',
+  overflowWrap: 'anywhere'
+}
+
+const likeButtonStyle = {
+  marginLeft: '1rem',
+  lineHeight: '.75rem'
+}
+
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const modifiedFullInfoStyle = {
+    ...fullInfoStyle,
+    display: visible ? 'flex' : 'none'
+  }
+
+  const modifiedPreviewInfoStyle = {
+    ...previewInfoStyle,
+    color: visible ? '#555' : '#000',
+    paddingLeft: visible ? '.5rem' : '0rem'
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <>
+      <div style={modifiedPreviewInfoStyle}>
+        <span>{blog.title} {blog.author}</span>
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+
+      <div style={modifiedFullInfoStyle}>
+        <span>{blog.url}</span>
+        <span>
+          {blog.likes}
+          <button style={likeButtonStyle}>like</button>
+        </span>
+        <span>{blog.user.name}</span>
+      </div>
+    </>
+  )
+}
 export default Blog
